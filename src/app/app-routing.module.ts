b9b538df81d3b3a52cc72ae70a05726efd440503
Saul Alvarez <anderssonsaul@gmail.com>
2022-10-22 02:49:19 -0600

    se agrega componente vuelo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AgregarComponent } from './Avion/agregar/agregar.component';
 import { EditarComponent } from './Avion/editar/editar.component';
 import { AsientoComponent } from './Avion/asiento/asiento.component';
 import { SeatsComponent } from './Avion/seats/seats.component';
+import { VueloComponent } from './Avion/vuelo/vuelo.component';
 
 const routes: Routes = [
 {path:'listar', component:ListarComponent},
 {path:'agregar',component:AgregarComponent},
 {path:'editar', component:EditarComponent},
 {path:'asiento', component:AsientoComponent},
-{path:'seats/:idAvion', component:SeatsComponent}
+{path:'seats/:idAvion', component:SeatsComponent},
+{path:'vuelo', component:VueloComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './Avion/listar/listar.component';
 import { AgregarComponent } from './Avion/agregar/agregar.component';
 import { EditarComponent } from './Avion/editar/editar.component';
-
-
-import{FormsModule}from '@angular/forms'
+import{FormsModule, ReactiveFormsModule}from '@angular/forms'
 import{ServiceService} from '../app/Service/service.service'
 import{HttpClientModule} from '@angular/common/http';
 import { AsientoComponent } from './Avion/asiento/asiento.component';
 import { SeatsComponent } from './Avion/seats/seats.component'
+import { MatSelectModule } from '@angular/material/select';
+import { VueloComponent } from './Avion/vuelo/vuelo.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +20,16 @@ import { SeatsComponent } from './Avion/seats/seats.component'
     AgregarComponent,
     EditarComponent,
     AsientoComponent,
-    SeatsComponent
+    SeatsComponent,
+    VueloComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSelectModule,
   ],
   providers: [ServiceService],
   bootstrap: [AppComponent]
